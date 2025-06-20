001f6c23a4ec967062458c2caf52b3cc323b1230
Naveen-kkkk <naveenkumarbalsamand123@gmail.com>
2025-06-20 16:43:06 +0530

    3.InterfaceBodyExample

---
+++ src/InterfaceBodyExample.java
@@ -0,0 +1,21 @@
+
+interface Tester {
+    public static void disp() {
+        System.out.println("Hello");
+    }
+
+    public default void print() {
+        System.out.println("Byee");
+    }
+}
+
+public class InterfaceBodyExample {
+    public static void main(String[] args) {
+        //Anonymus Inner Class
+        Tester tester = new Tester() {};
+        tester.print();
+        Tester.disp();
+    }
+}
+//Byee
+//Hello
